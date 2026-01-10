b7aabe52353f9c63c4893bead168d706db161efb
Tauhid3K <ts.ek.3902@gmail.com>
2026-01-11 00:19:13 +0600

    added comment

--- p03_01Exr.py
+++ p03_01Exr.py
@@ -1,3 +1,6 @@
+#game: Mad Libs
+#description: A simple Mad Libs game that prompts the user for words and constructs a story
+
 adjective = input("Enter an adjective: ")
 noun = input("Enter a noun: ")
 adjective2 = input("Enter another adjective: ")
@@ -6,4 +9,4 @@ adjective3 = input("Enter one more adjective: ")
 print(f"Today I went to the {adjective} market")
 print(f"In the market I saw a good {noun}")
 print(f"{noun} was very {adjective2}")
-print(f"I was {adjective3}")
+print(f"I was {adjective3}") 
