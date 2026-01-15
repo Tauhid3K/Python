f5a7fa23636f4432056581a75a85cc9e275fecb8
Tauhid3K <ts.ek.3902@gmail.com>
2026-01-15 15:51:33 +0600

    modified the weight converter

--- p07_Weight_Converter.py
+++ p07_Weight_Converter.py
@@ -4,10 +4,10 @@ unit = input("Kilograms or Pounds? (K/L): ")
 if unit == 'K' or unit == 'k':
     weight = weight * 2.205
     unit = 'Lbs.'
+    print(f"Your weight is {weight} {unit}")
 elif unit == 'L' or unit == 'l':
     weight = weight / 2.205
     unit = 'kgs.'
+    print(f"Your weight is {weight} {unit}")
 else:
-    print(f"{unit} was not valid")
-    
-print(f"Your weight is {weight} {unit}")
+    print(f"{unit} is not valid")
